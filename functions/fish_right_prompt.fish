30678f480febb9ac927252828e7c82d557132f29
Matthew Brock <mtbrock1@gmail.com>
2018-01-08 20:23:53 -0500

    Updated several functions

---
+++ functions/fish_right_prompt.fish
@@ -0,0 +1,6 @@
+# Defined in /home/matt/.local/share/omf/themes/default/fish_right_prompt.fish @ line 1
+function fish_right_prompt
+	#set_color $fish_color_autosuggestion ^/dev/null; or set_color 555
+  #date "+%H:%M:%S"
+  #set_color normal
+end
